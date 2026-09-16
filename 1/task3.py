62a0d9bd03fd8aba49ddaa3f8b134cce58eb990f
Weathermaker4 <145537175+Weathermaker4@users.noreply.github.com>
2026-09-16 12:49:29 +0300

    Create task3.py

---
+++ 1/task3.py
@@ -0,0 +1,15 @@
+# abc_2026
+# Федосов Артем
+#
+# todo: Данные две переменные:
+# age = 36.6
+# temperature = 25
+# Нужно обменять значения переменных местами. В итого age
+# должен равнятся 25 а temperature – 36.6:
+
+
+age = 36.6
+temperature = 25
+age, temperature = temperature, age
+print(f"age = {age}")
+print(f"temperature = {temperature}")
