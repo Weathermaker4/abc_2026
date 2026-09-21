06e4b812a456299a1b521976da7e4b1928fe795f
Weathermaker4 <145537175+Weathermaker4@users.noreply.github.com>
2026-09-21 07:36:34 +0300

    Create task17.py

---
+++ 3/task17.py
@@ -0,0 +1,23 @@
+# abc_2026
+# Fedosov Artem
+
+
+#todo: Заданы множества
+# все пользователи
+all_users = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+
+# пользователи не в сети
+offline_users = {'id3', 'id9', 'id7', 'id2', 'id4', 'id6'}
+
+#Вычислить пользователей online
+
+
+# Заданы множества
+all_users = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+offline_users = {'id3', 'id9', 'id7', 'id2', 'id4', 'id6'}
+
+# Вычитаем из всех пользователей тех, кто сейчас оффлайн
+online_users = all_users - offline_users
+
+# Выводим результат
+print("Пользователи онлайн:", online_users)
